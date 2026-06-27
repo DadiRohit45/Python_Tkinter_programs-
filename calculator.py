8907f7d2ca8c22a7d42663f59cbf3fcde1e703aa
Dadi Rohit <157350734+DadiRohit45@users.noreply.github.com>
2026-06-27 13:09:21 +0530

    added a tkinter calculator

---
+++ calculator.py
@@ -0,0 +1,82 @@
+from tkinter import *
+t=Tk()
+t.title("calculator")
+E1=Entry(t,bg="black",fg="white",width=22,font=("Arial",17),bd=12)
+E1.grid(row=0,column=0,columnspan=4)
+expression=""
+#result_shown=False    
+def calculation():
+    global expression#,result_shown
+    try:
+        result=str(eval(expression))
+        E1.delete(0,END)
+        E1.insert(0,result)
+        expression=result
+        #result_shown=True
+    except:
+        E1.delete(0,END)
+        E1.insert(0,'ERROR')
+        expression=""
+
+def click(a):
+    global expression,result_shown
+    #if result_shown==True:
+        #E1.delete(0,END)
+        #E1.insert(0,'ans')
+        #sresult_shown=False
+    expression+=str(a)
+    E1.insert(END,a)
+    
+def clear():
+    global expression,result_shown
+    expression=""
+    E1.delete(0,END)
+    #result_shown=False
+    
+def remove():
+    global expression
+    l=len(E1.get())-1
+    E1.delete(l)
+    expression=expression[:-1]
+
+b1=Button(t,text='1',bg="black",fg="white",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click(1),cursor="hand2")
+b1.grid(row=4,column=0)
+b1=Button(t,text='2',bg="black",fg="white",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click(2),cursor="hand2")
+b1.grid(row=4,column=1)
+b1=Button(t,text='3',bg="black",fg="white",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click(3),cursor="hand2")
+b1.grid(row=4,column=2)
+b1=Button(t,text='+',bg="black",fg="orange",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click('+'),cursor="hand2")
+b1.grid(row=4,column=3)
+b1=Button(t,text='4',bg="black",fg="white",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click(4),cursor="hand2")
+b1.grid(row=3,column=0)
+b1=Button(t,text='5',bg="black",fg="white",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click(5),cursor="hand2")
+b1.grid(row=3,column=1)
+b1=Button(t,text='6',bg="black",fg="white",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click(6),cursor="hand2")
+b1.grid(row=3,column=2)
+b1=Button(t,text='—',bg="black",fg="orange",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click('-'),cursor="hand2")
+b1.grid(row=3,column=3)
+b1=Button(t,text='7',bg="black",fg="white",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click(7),cursor="hand2")
+b1.grid(row=2,column=0)
+b1=Button(t,text='8',bg="black",fg="white",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click(8),cursor="hand2")
+b1.grid(row=2,column=1)
+b1=Button(t,text='9',bg="black",fg="white",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click(9),cursor="hand2")
+b1.grid(row=2,column=2)
+b1=Button(t,text='/',bg="black",fg="orange",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click('/'),cursor="hand2")
+b1.grid(row=2,column=3)
+b1=Button(t,text='0',bg="black",fg="white",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click(0),cursor="hand2")
+b1.grid(row=5,column=0)
+b1=Button(t,text='x',bg="black",fg="orange",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click('*'),cursor="hand2")
+b1.grid(row=5,column=2)
+b1=Button(t,text='=',bg="black",fg="orange",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:calculation(),cursor="hand2")
+b1.grid(row=6,column=1)
+b1=Button(t,text='%',bg="black",fg="orange",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click("%"),cursor="hand2")
+b1.grid(row=5,column=3)
+b1=Button(t,text='AC',bg="black",fg="orange",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:clear(),cursor="hand2")
+b1.grid(row=6,column=2)
+b1=Button(t,text='•',bg="black",fg="white",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click("."),cursor="hand2")
+b1.grid(row=5,column=1)
+b1=Button(t,text='⌫',bg="black",fg="orange",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:remove(),cursor="hand2")
+b1.grid(row=6,column=3)
+b1=Button(t,text='e',bg="black",fg="orange",font=("Arial",10),bd=7,height=5,width=7,activebackground="grey",command=lambda:click("error"),cursor="hand2")
+b1.grid(row=6,column=0)
+mainloop()
